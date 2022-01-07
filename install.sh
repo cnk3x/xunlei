b489e256aedf5f88a0d6efc72befe13d81e4bad5
wen <wen@k3x.cn>
2022-01-08 01:11:12 +0800

    scripts

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-latest=$(curl -fsSL https://gh.k3x.cn/api/repos/cnk3x/xunlei-from-syno/releases/latest | grep browser_download_url | grep $(uname -m) | head -n 1 | grep -Eo https.+.tar.gz | sed 's/github.com/gh.k3x.cn/g')
+latest=$(curl -fsSL https://gh.k3x.cn/api/repos/cnk3x/xunlei/releases/latest | grep browser_download_url | grep $(uname -m) | head -n 1 | grep -Eo https.+.tar.gz | sed 's/github.com/gh.k3x.cn/g')
 curl -fsSL ${latest} | tar zx
 ./xunlei $@
 
