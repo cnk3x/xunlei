f240af6f870141fd29494ca470707f31c83c2840
wen <wen@k3x.cn>
2022-03-16 11:00:24 +0800

    update to 2.4.5

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-latest=$(curl -fsSL https://gh.k3x.cn/api/repos/cnk3x/xunlei/releases/latest | grep browser_download_url | grep $(uname -m) | head -n 1 | grep -Eo https.+.tar.gz | sed 's/github.com/gh.k3x.cn/g')
+latest=$(curl -fsSL https://api.github.com/repos/cnk3x/xunlei/releases/latest | grep browser_download_url | grep $(uname -m) | head -n 1 | grep -Eo https.+.tar.gz | sed 's|github.com|mirror.ghproxy.com/https://github.com|g')
 echo "download: $latest"
 curl -fsSL ${latest} | tar zx
 ./xunlei $@
