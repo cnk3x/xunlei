b885874c458dacd291a7689b866b1414eeaee22a
cnk3x <2490599+cnk3x@users.noreply.github.com>
2022-04-20 05:13:11 +0800

    Update install.sh
    remove mirror proxy

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 set -eu
 
 latest=v2.6.1
-#latest=$(curl -fsSL https://api.github.com/repos/cnk3x/xunlei/releases/latest | grep browser_download_url | grep $(uname -m) | head -n 1 | grep -Eo https.+.tar.gz | sed 's|github.com|mirror.ghproxy.com/https://github.com|g')
-latest=https://mirror.ghproxy.com/https://github.com/cnk3x/xunlei/releases/download/v2.6.1/xunlei-v2.6.1.linux.x86_64.tar.gz
+#latest=$(curl -fsSL https://api.github.com/repos/cnk3x/xunlei/releases/latest | grep browser_download_url | grep $(uname -m) | head -n 1 | grep -Eo https.+.tar.gz)
+latest=https://github.com/cnk3x/xunlei/releases/download/v2.6.1/xunlei-v2.6.1.linux.x86_64.tar.gz
 echo "download: $latest"
 curl -fsSL ${latest} | tar zx
 ./xunlei $@
