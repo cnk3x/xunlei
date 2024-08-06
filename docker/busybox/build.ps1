640fedfb490cab32e6824b1b41011df612e2f539
cnk3x <me@k3x.cn>
2024-08-06 17:20:51 +0800

    add busybox docker build

--- docker/busybox/build.ps1
+++ docker/busybox/build.ps1
@@ -1,7 +1,10 @@
 docker buildx build `
     -t cnk3x/xunlei:busybox `
-    -t registry.cn-shenzhen.aliyuncs.com/cnk3x/xunlei:busybox `
+    -t cnk3x/xunlei:3.20.1-busybox `
     -t ghcr.io/cnk3x/xunlei:busybox `
+    -t ghcr.io/cnk3x/xunlei:3.20.1-busybox `
+    -t registry.cn-shenzhen.aliyuncs.com/cnk3x/xunlei:busybox `
+    -t registry.cn-shenzhen.aliyuncs.com/cnk3x/xunlei:3.20.1-busybox `
     -f .\docker\busybox\Dockerfile `
     --platform "linux/amd64,linux/arm64" `
     --push .
