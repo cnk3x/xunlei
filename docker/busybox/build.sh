90d4017c4b07c27d37eaaf8187a6a96b23df5ab1
cnk3x <me@k3x.cn>
2024-08-06 17:14:31 +0800

    add busybox docker build

---
+++ docker/busybox/build.sh
@@ -0,0 +1,7 @@
+docker buildx build \
+  -t cnk3x/xunlei:busybox \
+  -t registry.cn-shenzhen.aliyuncs.com/cnk3x/xunlei:busybox \
+  -t ghcr.io/cnk3x/xunlei:busybox \
+  -f ./docker/busybox/Dockerfile \
+  --platform "linux/amd64,linux/arm64" \
+  --push .
